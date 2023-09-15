7ec49e237b63a0a8205032a4060108b186d9817c
Melih Gunduz <melih.gunduz@hotmail.com>
2023-09-15 12:51:27 +0300

    Hardhat configured
    Networks and solidity version added. Files got ready for test.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+const {MNEMONIC} = require('./secrets.json');
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: MNEMONIC}
+    },
+  }
 };
 
 export default config;
